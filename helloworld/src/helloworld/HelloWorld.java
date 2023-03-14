6aa563c80430711b0d5203a2df303ffa799529aa
Yngrid Padilha <yngrid_padilha@hotmail.com>
2023-03-14 12:00:28 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	int teste = 10;
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+		System.out.println("\nOlá Turma 62!");
+
+	}
+
+}
