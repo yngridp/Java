a5398829e299a1778f8172d4d332bd81faf019d8
Yngrid Padilha <yngrid_padilha@hotmail.com>
2023-03-15 10:44:52 -0300

    Aula_01

---
+++ aula_01/src/aula_01/Aula01.java
@@ -0,0 +1,43 @@
+package aula_01;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Aula01 {
+	
+	public static void main(String [] args) {
+		
+		//Formatação dos Números decimais
+		DecimalFormat df = new DecimalFormat("###,###,##0.00");
+		
+		/* Leitura de dados via teclado */
+		Scanner leia = new Scanner(System.in);
+		
+		double numero1, numero2;
+		String nome;
+		
+		System.out.println("Digite o seu nome: ");
+		nome = leia.nextLine();
+		
+		
+		System.out.println("Digite o primeiro numero: ");
+		
+		numero1 = leia.nextDouble();
+		
+		System.out.println("Digite o segundo numero: ");
+		numero2 = leia.nextDouble();
+		
+		System.out.println("Soma: " + df.format ((numero1 + numero2)));
+		System.out.println("Subtração: " + df.format((numero1 - numero2)));
+		System.out.println("Multiplicação: " + df.format((numero1 * numero2)));
+		System.out.println("Divisão: " + df.format((numero1 / numero2)));
+		System.out.println("Potência: " + df.format(Math.pow(numero1, numero2)));
+		System.out.println("Raíz Quadrada: " + df.format(Math.sqrt(numero1)));
+		
+		System.out.println("Nome: "+ nome);
+		
+		leia.close();
+		
+	}
+
+}
--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -2,10 +2,12 @@ package helloworld;
 
 public class HelloWorld {
 
-	int teste = 10;
+	static int valor = 10;
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo");
 		System.out.println("\nOlá Turma 62!");
+		System.out.println("valor da variável:" + valor);
+		
 
 	}
 
