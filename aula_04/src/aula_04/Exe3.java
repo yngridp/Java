cb91b0cf0c2cf7801d8828acdb827a7d5bae52b2
Yngrid Padilha <yngrid_padilha@hotmail.com>
2023-03-20 21:40:02 -0300

    aula_04

--- aula_04/src/aula_04/Exe3.java
+++ aula_04/src/aula_04/Exe3.java
@@ -1,36 +1,51 @@
-/*package aula_04;
+package aula_04;
 
 import java.util.Scanner;
 
 public class Exe3 {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new scanner(System.in);
-		
-		int [][] matriz = new int [3][3];
-		int somaPrincipal = 0, somaSecundaria =0;
-		String diagonalPrincipal ="" , diagonalSecundaria = "";
-		
-		for(int iLinha = 0; iLinha < matriz.length; iLinha ++) {
-			
-			for(int iColuna = 0; iColuna < matriz.length; iColuna ++) {
-				
+
+		Scanner leia = new Scanner(System.in);
+
+		int[][] matriz = new int[3][3];
+		int somaPrincipal = 0, somaSecundaria = 0;
+		String diagonalPrincipal = "", diagonalSecundaria = "";
+
+		for (int iLinha = 0; iLinha < matriz.length; iLinha++) {
+
+			for (int iColuna = 0; iColuna < matriz.length; iColuna++) {
+
 				System.out.println("Digite um número: ");
-				matriz[iLinha][iColuna] = scanner.nextInt();
-				
+				matriz[iLinha][iColuna] = leia.nextInt();
+
 			}
 		}
-		for(int contador = 0; contador < matriz.length; contador ++) {
-			
+
+		for (int contador = 0; contador < matriz.length; contador ++) {
+
 			diagonalPrincipal += matriz[contador][contador] + " ";
 			somaPrincipal += matriz[contador][contador];
 			
 			diagonalSecundaria += matriz[contador][matriz.length - 1 - contador] + " ";
 			somaSecundaria += matriz[contador][matriz.length - 1 - contador];
-			
+		
 		}
+		
+		System.out.println("Elementos da Diagonal Principal:\n");
+		System.out.println(diagonalPrincipal);
+
+		System.out.println("\n\nElementos da Diagonal Secundária:\n");
+		System.out.println(diagonalSecundaria);
+
+		System.out.println("\n\nSoma de todos Elementos da Diagonal Principal:\n");
+		System.out.println(somaPrincipal);
+
+		System.out.println("\n\nSoma de todos Elementos da Diagonal Secundária:\n");
+		System.out.println(somaSecundaria);
+
+		leia.close();
+		
 	}
 
 }
-*/
--- aula_04/src/aula_04/Matriz.java
+++ aula_04/src/aula_04/Matriz.java
@@ -1,14 +1,37 @@
 package aula_04;
 
+import java.util.Scanner;
+
 public class Matriz {
 
 	public static void main(String[] args) {
 		
-	/*	int [][] matrizInteiros = {{10,15,35}, {70,120,140},{50,100,150}};
-
-		float matrizFloat [][] = new float [2][3];
+		Scanner leia = new Scanner(System.in);
 		
-		for(int iLinha =0; < matrizInteiro.>)*/
+		int[][] matrizInteiros = {{10, 15, 35}, {70, 120, 140}, {50, 100, 150}};
+
+		float matrizFloat[][] = new float[2][3];
+		
+		for (int iLinha = 0; iLinha < matrizInteiros.length; iLinha ++) {
+			for (int iColuna = 0; iColuna < matrizInteiros.length; iColuna ++) {
+				System.out.println(matrizInteiros[iLinha][iColuna]);
+			}
+		}
+		
+		for (int iLinha = 0; iLinha < matrizFloat.length; iLinha ++) {
+			for (int iColuna = 0; iColuna < matrizFloat[iLinha].length; iColuna ++) {
+				System.out.println("Digite um numero: ");
+				matrizFloat[iLinha][iColuna] = leia.nextFloat();
+			}
+		}
+		
+		for (int iLinha = 0; iLinha < matrizFloat.length; iLinha ++) {
+			for (int iColuna = 0; iColuna < matrizFloat[iLinha].length; iColuna ++) {
+				System.out.println(matrizFloat[iLinha][iColuna]);
+			}
+		}
+		
+		leia.close();
 	}
 
 }
